7a060c1265b5747bec6981bfa2c4a87ac10b6a89
David Thompson <deetee81@gmail.com>
2020-05-28 23:57:47 +0000

    added functionality to the add currency button using JavaScript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,11 @@
 // Global Variables 
 
 const currencyBtn = document.querySelector(".currency-btn");
+
+// Event Listeners
+
+currencyBtn.addEventListener("click", currencyBtnClick);
+
+function currencyBtnClick(event) {
+    currencyBtn.classList.toggle("open");
+}
